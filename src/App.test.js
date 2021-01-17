713d70e1478479d93aef61f451e5bd331b193d14
Binh Nguyen <b.nguyen.sn@gmail.com>
2021-01-17 12:46:25 +0000

    fix failing tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { apolloClient } from './apolloClient';
+import { ApolloProvider } from '@apollo/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+describe('app test', () => {
+  const queryClient = new QueryClient();
+
+  it('renders the app correctly', () => {
+    render(
+      <ApolloProvider client={apolloClient}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ApolloProvider>
+    );
+
+    expect(screen.getByRole('main')).toBeInTheDocument();
+  });
 });
